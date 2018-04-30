2f4dc341bdf4a0c71385e1eed6b1aae85074f6de
bdowds <bwdowds@gmail.com>
2018-04-30 13:53:01 -0500

    Added Interface

---
+++ SqlIntro/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlIntro
+{
+    interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        void DeleteProduct(int id);
+        void UpdateProduct(Product prod);
+        void InsertProduct(Product prod);
+    }
+}
