953abee2b380cdd6cd90284b5522fd581b862d7b
bdowds <bwdowds@gmail.com>
2018-05-01 08:41:16 -0500

    Added Statement to Program that grabs the Reviews

--- SqlIntro/Program.cs
+++ SqlIntro/Program.cs
@@ -9,11 +9,16 @@ namespace SqlIntro
             var connectionString = "Server=localhost;Database=adventureworks;Uid=root;Pwd=password;";
             var repo = new DapperProductRepository(connectionString);
 
+            /*
             foreach (var prod in repo.GetProducts())
             {
                 Console.WriteLine("Product Name:" + prod.Name);
             }
-            
+            */
+            foreach (var prod in repo.GetProductsWithReview())
+            {
+                Console.WriteLine("Product Name:" + prod.Name + "\n\nReview: " + prod.Review + "\n\n");
+            }
             Console.ReadLine();
         }
     }
